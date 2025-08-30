77cb9b5798171acbdf92934e7f916f6da7228f6a
oSheep <ohsheepp@gmail.com>
2025-08-30 11:32:00 -0700

    Update article display to 5x2 grid with no image

--- src/components/TopArticles.tsx
+++ src/components/TopArticles.tsx
@@ -17,26 +17,33 @@ export default function TopArticles() {
 	}, []);
 
 	return (
-		<>
-			<div className="grid grid-cols-4 gap-3 mb-16 border-b-2 border-gray-200">
-				{/* Big Image - spans 2x3 */}
-				<div className="col-span-2 row-span-2 relative rounded-xl overflow-hidden">
-					<img
-						src={Image}
-						alt="Featured News"
-						className="w-full h-full object-cover"
-					/>
-					<div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white text-xl font-bold">
-						"Reading satire news is like getting your veggies in cake
-						form—tasty, fun, and surprisingly informative." — Albert Mewstein
-					</div>
-				</div>
+		// <>
+		// 	<div className="grid grid-cols-4 gap-3 mb-16 border-b-2 border-gray-200">
+		// 		{/* Big Image - spans 2x3 */}
+		// 		<div className="col-span-2 row-span-2 relative rounded-xl overflow-hidden">
+		// 			<img
+		// 				src={Image}
+		// 				alt="Featured News"
+		// 				className="w-full h-full object-cover"
+		// 			/>
+		// 			<div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4 text-white text-xl font-bold">
+		// 				"Reading satire news is like getting your veggies in cake
+		// 				form—tasty, fun, and surprisingly informative." — Albert Mewstein
+		// 			</div>
+		// 		</div>
 
-				{topTenArticles &&
-					topTenArticles.map((article) => (
-						<NewsCard key={`top-${article.id}`} articleInfo={article} />
-					))}
-			</div>
-		</>
+		// 		{topTenArticles &&
+		// 			topTenArticles.map((article) => (
+		// 				<NewsCard key={`top-${article.id}`} articleInfo={article} />
+		// 			))}
+		// 	</div>
+		// </>
+		<div className="grid grid-cols-5 grid-rows-2">
+			{topTenArticles.map((article, index) => (
+				<div>
+					<span className="font-bold">{index + 1 + ". " + article.title}</span>
+				</div>
+			))}
+		</div>
 	);
 }
