29a93e811543c2be6e9b8b3c1d82a51527baf5e7
oSheep <ohsheepp@gmail.com>
2025-09-21 10:51:44 -0700

    increment article view on clicking into article

--- src/components/news/NewsHeroCard.tsx
+++ src/components/news/NewsHeroCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 import type { ArticleInfo } from "@/types/articleTypes";
+import { incrementArticleViewed } from "@/api/articleApi";
 
 interface NewsCardProp {
 	articleInfo: ArticleInfo;
@@ -11,7 +12,12 @@ export default function NewsHeroCard({ articleInfo, small }: NewsCardProp) {
 	return (
 		<div className={`${small ? "h-24" : "h-48"}`}>
 			<h3 className="text-lg font-semibold text-gray-800 hover:text-amber-600 cursor-pointer">
-				<Link to={`/article/${articleInfo.id}`}>{articleInfo.title}</Link>
+				<Link
+					to={`/article/${articleInfo.id}`}
+					onClick={() => incrementArticleViewed(articleInfo.id)}
+				>
+					{articleInfo.title}
+				</Link>
 			</h3>
 			{!small && articleInfo.summary && (
 				<span className="text-sm">{articleInfo.summary}</span>
