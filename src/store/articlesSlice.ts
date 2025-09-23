c7ed052c6a12c443519dcce1c5ea9c833c6c3be3
oSheep <ohsheepp@gmail.com>
2025-09-22 23:34:01 -0700

    remove commented out lines

--- src/store/articlesSlice.ts
+++ src/store/articlesSlice.ts
@@ -37,37 +37,6 @@ const initialState: ArticlesState = {
 	error: undefined,
 };
 
-// export const getArticlesInfo = createAsyncThunk<ArticleInfo[]>(
-// 	"articles/getArticlesInfo",
-// 	async () => {
-// 		const response = await fetch("http://localhost:3001/article-info?page=1&limit=10", {
-// 			method: "GET",
-// 			headers: { "Content-Type": "application/json" },
-// 		});
-// 		if (!response.ok) {
-// 			throw new Error(`Error: ${response.statusText}`);
-// 		}
-// 		const data = await response.json();
-//     return data.map(articleInfoTransform)
-// 	}
-// );
-
-// export const getArticleDetail = createAsyncThunk<ArticleDetail, string>(
-// 	"articles/getArticleDetail",
-// 	async (articleId) => {
-// 		const response = await fetch("http://localhost:3001/article-detail", {
-// 			method: "POST",
-// 			headers: { "Content-Type": "application/json" },
-// 			body: JSON.stringify({ id: articleId }),
-// 		});
-// 		if (!response.ok) {
-// 			throw new Error(`Error: ${response.statusText}`);
-// 		}
-//     const data = await response.json()
-//     return articleDetailTransform(data)
-// 	}
-// );
-
 export const loadArticlesInfo = createAsyncThunk<ArticleInfo[]>(
 	"articles/getArticlesInfo",
 	async () => {
