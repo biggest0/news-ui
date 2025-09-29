004912df8f89303ed74f98138ccbbee19b623e29
biggest0 <thebiggestkai@gmail.com>
2025-09-29 09:11:42 -0700

    modularize TopArticles component and change name to TopArticleSection

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import NewsSection from "@/components/news/NewsSection";
-import TopArticles from "@/components/news/TopArticles";
+import TopArticlesSection from "@/components/news/TopArticlesSection";
 import NewsHero from "@/components/news/NewsHero";
 import BackToTopButton from "@/components/common/BackToTopButton";
 
@@ -7,7 +7,7 @@ export default function HomePage() {
 	return (
 		<>
 			<NewsHero />
-			<TopArticles />
+			<TopArticlesSection />
 			<NewsSection key={"home-article-section"} />
 			<BackToTopButton />
 		</>
