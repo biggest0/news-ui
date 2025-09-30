488345286d97c6caff78280dfc94c13ad4e52d9b
biggest0 <thebiggestkai@gmail.com>
2025-09-30 12:08:12 -0700

    update AppTitle font for small, medium, large screens

--- src/components/navBar/AppTitle.tsx
+++ src/components/navBar/AppTitle.tsx
@@ -5,8 +5,8 @@ interface AppTitleProps {
 export const AppTitle = ({ variant = "desktop" }: AppTitleProps) => {
 	const tailwindClasses =
 		variant === "desktop"
-			? "absolute left-1/2 transform -translate-x-1/2 lg:text-5xl font-semibold text-gray-800 tracking-wide"
-			: "text-lg font-semibold text-gray-800 tracking-wide";
+			? "absolute left-1/2 transform -translate-x-1/2 text-3xl lg:text-5xl font-semibold text-gray-800 tracking-wide"
+			: "text-xl font-semibold text-gray-800 tracking-wide";
 
 	return <div className={tailwindClasses}>THE CATIRE TIMES</div>;
 };
