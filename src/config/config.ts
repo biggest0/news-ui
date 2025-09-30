1f4a285a84ab2f5368aaf090e31a148f2b781160
biggest0 <thebiggestkai@gmail.com>
2025-09-30 16:37:25 -0700

    v0.0.0

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 // const URL = "http://localhost:3001";
 export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
-export const VERSION = "0.0.0"
+export const VERSION = "0.0.0";
