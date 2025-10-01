6b1f5000829c1b90ae4f4ca61fbfd21b5833fcf5
biggest0 <thebiggestkai@gmail.com>
2025-09-30 22:05:55 -0700

    rename app to Catire Time

--- src/components/navBar/AppTitle.tsx
+++ src/components/navBar/AppTitle.tsx
@@ -8,5 +8,5 @@ export const AppTitle = ({ variant = "desktop" }: AppTitleProps) => {
 			? "absolute left-1/2 transform -translate-x-1/2 text-3xl lg:text-5xl font-semibold text-gray-800 tracking-wide"
 			: "text-xl font-semibold text-gray-800 tracking-wide";
 
-	return <div className={tailwindClasses}>THE CATIRE TIMES</div>;
+	return <div className={tailwindClasses}>CATIRE TIME</div>;
 };
