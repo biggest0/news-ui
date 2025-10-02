88aa7dfc2c8000bcf0c165e63ed4acacaa08a206
biggest0 <thebiggestkai@gmail.com>
2025-10-01 20:06:01 -0700

    update text and styling to match parent components

--- src/components/common/UnderMaintenance.tsx
+++ src/components/common/UnderMaintenance.tsx
@@ -2,7 +2,7 @@ import Image from "@/assets/maintenance_hero_image.jpg";
 
 export const UnderMaintenance = () => {
 	return (
-		<div className="flex flex-col items-center text-center py-6 border-b border-gray-400">
+		<div className="flex flex-col items-center text-center pt-4">
 			<img
 				src={Image}
 				alt="Cats at work"
@@ -11,7 +11,7 @@ export const UnderMaintenance = () => {
 			<h2 className="text-xl md:text-3xl font-bold text-gray-800">
 				This page is still in purr-duction 🐱🔧
 			</h2>
-			<p className="mt-2">Our kit-terns are hard at work!</p>
+			<p className="mt-2">Our team is hard at work!</p>
 		</div>
 	);
 };
