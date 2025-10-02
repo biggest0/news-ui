41f4dbd111a2f1c1665d52c87796c7f410787e07
biggest0 <thebiggestkai@gmail.com>
2025-10-02 07:47:06 -0700

    implement AccountPage component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ArticlePages from "@/pages/ArticlesPage";
 import SearchPage from "./pages/SearchPage";
 import About from "./pages/About";
 import Contact from "./pages/Conatct";
+import AccountPage from "./pages/AccountPage";
 
 import NoticeBar from "@/components/common/NoticeBar";
 import NavBar from "@/components/navBar/NavBar";
@@ -38,6 +39,7 @@ function App() {
 					<Route path="/search" element={<SearchPage />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/contact" element={<Contact />} />
+					<Route path="/account" element={<AccountPage />} />
 					<Route path="*" element={<div>Page not Found</div>} />
 				</Routes>
 			</main>
