1d25e0edab26e28171ffbb1cb8de5bd2af0b13d4
biggest0 <thebiggestkai@gmail.com>
2025-10-03 20:19:46 -0700

    rename field onClick to onLinkClick

--- src/components/navBar/MobileNavLink.tsx
+++ src/components/navBar/MobileNavLink.tsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 
 interface MobileNavLinkProps {
-  linkTo: string;
-  linkLabel: string;
-  onClick?: () => void;
+	linkTo: string;
+	linkLabel: string;
+	onLinkClick?: () => void;
 }
 
-export const MobileNavLink = ({linkTo, linkLabel, onClick}: MobileNavLinkProps) => {
+export const MobileNavLink = ({
+	linkTo,
+	linkLabel,
+	onLinkClick,
+}: MobileNavLinkProps) => {
 	return (
 		<Link
 			to={linkTo}
-			onClick={onClick}
+			onClick={onLinkClick}
 			className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md transition-colors"
 		>
 			{linkLabel}
