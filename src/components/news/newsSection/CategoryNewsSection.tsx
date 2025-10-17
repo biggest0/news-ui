f439ac147c2d8f19ccd3d307b24d96d00de99817
biggest0 <thebiggestkai@gmail.com>
2025-10-17 10:15:47 -0700

    add unfinished changes to loadMoreArticles to work with ArticleInfoRequest object

--- src/components/news/newsSection/CategoryNewsSection.tsx
+++ src/components/news/newsSection/CategoryNewsSection.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+
 import type { RootState, AppDispatch } from "@/store/store";
 import { loadArticlesInfoByCategory } from "@/store/articlesSlice";
 import { BaseNewsSection } from "./BaseNewsSection";
+import type { ArticleInfoRequest } from "@/types/articleTypes";
 
 export function CategoryNewsSection() {
 	const dispatch = useDispatch<AppDispatch>();
@@ -19,13 +21,17 @@ export function CategoryNewsSection() {
 		);
 	}, [selectedCategory, articles]);
 
-	const loadMoreArticles = (page: number) => {
-		dispatch(
-			loadArticlesInfoByCategory({
-				page,
-				category: selectedCategory,
-			})
-		);
+	const loadMoreArticles = (request: ArticleInfoRequest) => {
+		if (request.page) {
+			dispatch(
+				// using a separate route because store structure is different for category articles and home page articles
+				// maybe find a better way to structure this in the future
+				loadArticlesInfoByCategory({
+					page: request.page,
+					category: selectedCategory,
+				})
+			);
+		}
 	};
 
 	return (
