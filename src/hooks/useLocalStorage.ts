3860a1fcb2af5f8f4b7a22f79f0e66161d9b08fc
biggest0 <thebiggestkai@gmail.com>
2025-10-27 20:31:51 -0700

    add localStorage hook to get and update

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+// Custom hook for localStorage management
+export function useLocalStorage<T>(key: string, initialValue: T) {
+	const [storedValue, setStoredValue] = useState<T>(() => {
+		try {
+			const item = window.localStorage.getItem(key);
+			return item ? (JSON.parse(item) as T) : initialValue;
+		} catch (error) {
+			console.error("Error reading localStorage key “" + key + "”:", error);
+			return initialValue;
+		}
+	});
+
+	useEffect(() => {
+		try {
+			console.log(storedValue);
+			window.localStorage.setItem(key, JSON.stringify(storedValue));
+		} catch (error) {
+			console.error("Error setting localStorage key “" + key + "”: ", error);
+		}
+	}, [storedValue]);
+
+	return [storedValue, setStoredValue] as const;
+}
