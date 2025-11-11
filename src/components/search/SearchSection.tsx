72edfc3b74fafe284d9be03873205b7a5815eede
biggest0 <thebiggestkai@gmail.com>
2025-11-11 13:35:01 -0800

    refactor SearchPage and separate logic and components

---
+++ src/components/search/SearchSection.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { LuSearch } from "react-icons/lu";
+
+import type { AppDispatch } from "@/store/store";
+import { loadArticlesInfoBySearch } from "@/store/articlesSlice";
+import { buildSearchUrl } from "@/utils/searchUrlUtils";
+import DateRangeFilter from "./DateRangeFilter";
+import SortByFilter from "./SortByFilter";
+
+interface SearchSectionProps {
+	query: string;
+	dateRange: string;
+	sortBy: string;
+}
+
+export default function SearchSection({
+	query,
+	dateRange,
+	sortBy,
+}: SearchSectionProps) {
+	const dispatch = useDispatch<AppDispatch>();
+	const navigate = useNavigate();
+
+	const [input, setInput] = useState(query ?? "");
+
+	useEffect(() => {
+		setInput(query);
+	}, [query]);
+
+	const updateUrl = (q: string, dateRange: string, sort: string) => {
+		navigate(buildSearchUrl({ query: q, dateRange, sortBy: sort }));
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		if (input.trim()) {
+			dispatch(loadArticlesInfoBySearch({ page: 1, search: input }));
+			navigate(
+				buildSearchUrl({
+					query: input,
+					dateRange,
+					sortBy,
+				})
+			);
+		}
+	};
+
+	return (
+		<section className="w-full flex flex-col justify-center items-center py-4 h-48 bg-gray-50">
+			{/* Search bar */}
+			<form
+				onSubmit={handleSubmit}
+				className="flex items-center w-full max-w-md border-b overflow-hidden mb-4"
+			>
+				<input
+					type="text"
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					placeholder="Search articles..."
+					className="flex-grow py-2 outline-none text-gray-700 text-2xl"
+				/>
+				<button
+					type="submit"
+					className="text-gray-400 px-4 py-2 hover:text-black transition cursor-pointer"
+				>
+					<LuSearch />
+				</button>
+			</form>
+
+			{/* Filter bar */}
+			<div className="flex justify-start gap-8 w-full max-w-md text-sm text-gray-600">
+				<DateRangeFilter
+					value={dateRange}
+					onChange={(value) => updateUrl(query, value, sortBy)}
+				/>
+				<SortByFilter
+					value={sortBy}
+					onChange={(value) => updateUrl(query, dateRange, value)}
+				/>
+			</div>
+		</section>
+	);
+}
