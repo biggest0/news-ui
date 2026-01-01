5795a78770e234c85516f7dc506f0add0839d465
biggest0 <thebiggestkai@gmail.com>
2025-12-31 20:47:17 -0800

    update return type to work with new article info response object

--- src/service/articleService.ts
+++ src/service/articleService.ts
@@ -5,37 +5,66 @@ import {
 	fetchArticlesInfo,
 	fetchTopTenArticles,
 } from "../api/articleApi";
-import type { ArticleDetail } from "../types/articleTypes";
-import type { ArticleInfoQueryDTO } from "@/types/articleDto";
+import type { ArticleDetail, ArticleResponse } from "../types/articleTypes";
+import type {
+	ArticleInfoQueryDTO,
+	ArticleInfoResponseDTO,
+} from "@/types/articleDto";
 import {
 	mapDTOtoArticleDetail,
 	mapDTOtoArticleInfo,
 } from "@/mappers/articleMapper";
 
-export async function getArticlesByCategory(page: number, category: string) {
+export async function getArticlesByCategory(
+	page: number,
+	category: string
+): Promise<ArticleResponse> {
 	try {
-		const data = await fetchArticlesByCategory(page, category);
-		return data.map(mapDTOtoArticleInfo);
+		const data: ArticleInfoResponseDTO = await fetchArticlesByCategory(
+			page,
+			category
+		);
+		return {
+			articles: data.articles.map(mapDTOtoArticleInfo),
+			count: data.count,
+		};
 	} catch (error) {
 		console.error("[Error fetching articles by category]:", error);
+		return { articles: [], count: 0 };
 	}
 }
 
-export async function getArticlesBySearch(page: number, search: string) {
+export async function getArticlesBySearch(
+	page: number,
+	search: string
+): Promise<ArticleResponse> {
 	try {
-		const data = await fetchArticlesBySearch(page, search);
-		return data.map(mapDTOtoArticleInfo);
+		const data: ArticleInfoResponseDTO = await fetchArticlesBySearch(
+			page,
+			search
+		);
+		return {
+			articles: data.articles.map(mapDTOtoArticleInfo),
+			count: data.count,
+		};
 	} catch (error) {
 		console.error("[Error fetching articles by search]:", error);
+		return { articles: [], count: 0 };
 	}
 }
 
-export async function getArticlesInfo(request: ArticleInfoQueryDTO) {
+export async function getArticlesInfo(
+	request: ArticleInfoQueryDTO
+): Promise<ArticleResponse> {
 	try {
-		const data = await fetchArticlesInfo(request);
-		return data.map(mapDTOtoArticleInfo);
+		const data: ArticleInfoResponseDTO = await fetchArticlesInfo(request);
+		return {
+			articles: data.articles.map(mapDTOtoArticleInfo),
+			count: data.count,
+		};
 	} catch (error) {
 		console.error("[Error fetching article infos]:", error);
+		return { articles: [], count: 0 };
 	}
 }
 
