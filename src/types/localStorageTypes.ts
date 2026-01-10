63e866cf22fb1c1d526d1594f4067a75a38ec48f
biggest0 <thebiggestkai@gmail.com>
2026-01-10 01:06:52 -0800

    add AppSetting type for localStorage

--- src/types/localStorageTypes.ts
+++ src/types/localStorageTypes.ts
@@ -2,3 +2,21 @@ export interface UserInfo {
 	name?: string;
 	biography?: string;
 }
+
+export interface AppSetting {
+	darkMode: boolean;
+	homeLayout: HomeLayout;
+}
+
+export interface HomeLayout {
+	visible: VisibleSections;
+	expanded: ExpandedSections;
+}
+
+export interface VisibleSections {
+	newsSection: boolean;
+}
+
+export interface ExpandedSections {
+	newsSection: boolean;
+}
