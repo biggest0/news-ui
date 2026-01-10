fdb453a16ceeef0f86ee13dd555c5f3ba2e1a5c8
biggest0 <thebiggestkai@gmail.com>
2026-01-09 23:44:41 -0800

    add function to update app setting in local storage

--- src/service/localStorageService.ts
+++ src/service/localStorageService.ts
@@ -11,3 +11,17 @@ export function handleLocalStorageUpdate(
 	const updatedArticles = [clickedArticle, ...filterClickedArticle];
 	setArticleHistory(updatedArticles.slice(0, 100));
 }
+
+export function updateAppSettingInLocalStorage(
+	key: string,
+	value: string | number | boolean
+) {
+	try {
+		const appConfig = localStorage.getItem("app_setting");
+		const config = appConfig ? JSON.parse(appConfig) : {};
+		config[key] = value;
+		localStorage.setItem("app_setting", JSON.stringify(config));
+	} catch (error) {
+		console.error("Error updating app setting in localStorage:", error);
+	}
+}
