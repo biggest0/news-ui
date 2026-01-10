1529f1d57ccda2252c71a582aa9b45306949a225
biggest0 <thebiggestkai@gmail.com>
2026-01-10 09:59:26 -0800

    add more functions to update app setting in localstorage

--- src/service/localStorageService.ts
+++ src/service/localStorageService.ts
@@ -1,4 +1,12 @@
 import type { ArticleInfo } from "@/types/articleTypes";
+import type {
+	ExpandedSections,
+	VisibleSections,
+} from "@/types/localStorageTypes";
+import {
+	getAppSetting,
+	setAppSetting,
+} from "@/utils/storage/localStorageUtils";
 
 export function handleLocalStorageUpdate(
 	clickedArticle: ArticleInfo,
@@ -12,16 +20,49 @@ export function handleLocalStorageUpdate(
 	setArticleHistory(updatedArticles.slice(0, 100));
 }
 
-export function updateAppSettingInLocalStorage(
-	key: string,
-	value: string | number | boolean
+export function updateSectionVisibility(
+	key: keyof VisibleSections,
+	value: boolean
 ) {
 	try {
-		const appConfig = localStorage.getItem("app_setting");
-		const config = appConfig ? JSON.parse(appConfig) : {};
-		config[key] = value;
-		localStorage.setItem("app_setting", JSON.stringify(config));
+		const appSetting = getAppSetting();
+		appSetting.homeLayout.visible[key] = value;
+		setAppSetting(appSetting);
 	} catch (error) {
 		console.error("Error updating app setting in localStorage:", error);
+	}
+}
+
+export function updateSectionExpansion(
+	key: keyof ExpandedSections,
+	value: boolean
+) {
+	try {
+		const appSetting = getAppSetting();
+		appSetting.homeLayout.expanded[key] = value;
+		setAppSetting(appSetting);
+	} catch (error) {
+		console.error("Error updating app setting in localStorage:", error);
+	}
+}
+
+export function toggleDarkMode() {
+	try {
+		const appSetting = getAppSetting();
+		appSetting.darkMode = !appSetting.darkMode;
+		setAppSetting(appSetting);
+	} catch (error) {
+		console.error("Error toggling dark mode in localStorage:", error);
+	}
+}
+
+export function togglePagePagination() {
+	try {
+		const appSetting = getAppSetting();
+		const pagePagination = appSetting.homeLayout.pagePagination;
+		appSetting.homeLayout.pagePagination = !pagePagination;
+		setAppSetting(appSetting);
+	} catch (error) {
+		console.error("Error toggling news section view in localStorage:", error);
 	}
 }
