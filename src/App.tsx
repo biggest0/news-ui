e8c0423fcc7e12bf6ad59e054df389a0684f1c93
biggest0 <thebiggestkai@gmail.com>
2026-01-12 23:34:42 -0800

    wrap app with AppSettingProvider

--- src/App.tsx
+++ src/App.tsx
@@ -14,35 +14,38 @@ import Footer from "@/components/layout/footer/Footer";
 import ArticlePage from "./pages/ArticlePage";
 import ScrollToTop from "@/components/layout/navigation/ScrollToTop";
 import { ARTICLE_ROUTES } from "@/constants/routes";
+import { AppSettingProvider } from "./contexts/AppSettingContext";
 
 function App() {
 	return (
 		<BrowserRouter basename="/">
-			<Header />
-			<main className="w-full max-w-[1280px] min-h-screen mx-auto px-4">
-				<ScrollToTop />
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					{/* Article category pages */}
-					{ARTICLE_ROUTES.map((category) => (
-						<Route
-							key={category}
-							path={`/${category}`}
-							element={<ArticlePages />}
-						/>
-					))}
-					{/* Article pages */}
-					<Route path="/article/:id" element={<ArticlePage />} />
-					<Route path="/search" element={<SearchPage />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/account" element={<AccountPage />} />
-					<Route path="/disclaimer" element={<DisclaimerPage />} />
-					{/* Other routes */}
-					<Route path="*" element={<div>Page not Found</div>} />
-				</Routes>
-			</main>
-			<Footer />
+			<AppSettingProvider>
+				<Header />
+				<main className="w-full max-w-[1280px] min-h-screen mx-auto px-4">
+					<ScrollToTop />
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						{/* Article category pages */}
+						{ARTICLE_ROUTES.map((category) => (
+							<Route
+								key={category}
+								path={`/${category}`}
+								element={<ArticlePages />}
+							/>
+						))}
+						{/* Article pages */}
+						<Route path="/article/:id" element={<ArticlePage />} />
+						<Route path="/search" element={<SearchPage />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/contact" element={<Contact />} />
+						<Route path="/account" element={<AccountPage />} />
+						<Route path="/disclaimer" element={<DisclaimerPage />} />
+						{/* Other routes */}
+						<Route path="*" element={<div>Page not Found</div>} />
+					</Routes>
+				</main>
+				<Footer />
+			</AppSettingProvider>
 		</BrowserRouter>
 	);
 }
