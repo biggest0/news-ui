a84283899fd809c58547a4f8308eff1499c32c87
biggest0 <thebiggestkai@gmail.com>
2026-01-15 22:24:37 -0800

    add section constants

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -1,3 +1,11 @@
 // LocalStorage
 export const USER_INFO = 'user_info'
 export const USER_ARTICLE_HISTORY = 'user_article_history'
+
+// Sections
+export const SECTIONS = {
+  NEWS: 'newsSection',
+  EDITORS: 'editorsSection',
+  CAT_FACTS: 'catFactsSection',
+  TOP_TEN: 'topTenSection',
+}
