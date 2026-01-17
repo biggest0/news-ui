11dc7dabcd2f516cf13ea80b0fe4e28f00814a0d
biggest0 <thebiggestkai@gmail.com>
2026-01-16 19:01:47 -0800

    implement StaffPicksSection, remove unused imports

--- src/components/news/shared/NewsSideColumn.tsx
+++ src/components/news/shared/NewsSideColumn.tsx
@@ -1,54 +1,18 @@
-import { EditorCardHorizontal } from "@/components/layout/sideColumn/EditorCardHorizontal";
-import { CATIRE_EDITORS, CAT_FACTS } from "@/components/layout/sideColumn/constants";
-import { CatFactsCard } from "@/components/layout/sideColumn/CatFactsCard";
-import { SectionHeader } from "@/components/common/layout/SectionHeader";
-import { SELECTED_ARTICLES } from "../tempArticles";
-import { Link } from "react-router-dom";
-import { incrementArticleViewed } from "@/api/articleApi";
-import { handleLocalStorageUpdate } from "@/service/localStorageService";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
-import type { ArticleInfo } from "@/types/articleTypes";
-import { USER_ARTICLE_HISTORY } from "@/constants/keys";
 import EditorsSection from "../section/EditorsSection";
 import CatFactsSection from "../section/CatFactsSection";
+import StaffPicksSection from "../section/StaffPicksSection";
 
 export default function NewsSideColumn() {
-	const [articleHistory, setArticleHistory] = useLocalStorage<ArticleInfo[]>(
-		USER_ARTICLE_HISTORY,
-		[]
-	);
-	const selectedArticles = SELECTED_ARTICLES;
 
 	return (
 		<>
-			{/* our editors */}
+			{/* Cat editors */}
 			<EditorsSection />
 
-			{/* staff favorites, for future */}
-			<div className="border-b border-gray-400 pb-6 space-y-4">
-				<SectionHeader title="STAFF FAVORITES" />
+			{/* Staff picks, for future */}
+			<StaffPicksSection />
 
-				{selectedArticles.slice(0, 5).map((article) => (
-					<div key={`side-${article.id}`}>
-						<Link
-							to={`/article/${article.id}`}
-							className="py-2 hover:text-amber-600 transition-colors duration-200 cursor-pointer"
-							onClick={() => {
-								incrementArticleViewed(article.id);
-								handleLocalStorageUpdate(
-									article,
-									articleHistory,
-									setArticleHistory
-								);
-							}}
-						>
-							{article.title}
-						</Link>
-					</div>
-				))}
-			</div>
-
-			{/* cat facts */}
+			{/* Cat facts */}
 			<CatFactsSection />
 
 			{/* cat merch */}
