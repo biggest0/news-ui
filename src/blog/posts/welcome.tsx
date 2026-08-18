31e3cc94672dc37dff0a6e093ebf3505abfcc0f7
biggest0 <thebiggestkai@gmail.com>
2026-08-18 11:05:21 -0700

    docs: update welcome post to clarify content focus and publishing frequency

--- src/blog/posts/welcome.tsx
+++ src/blog/posts/welcome.tsx
@@ -13,18 +13,18 @@ export default function WelcomePost() {
 	return (
 		<>
 			<p>
-				This is a small corner of Catire Time where I'll be writing about
-				the design and engineering decisions behind the site.
+				This is a small corner of Catire Time for the things that don't fit in
+				an article: how we pick the stories, why we write them the way we do,
+				and what we're working on next.
 			</p>
 			<p>
-				Posts here won't be frequent. I'll publish whenever something
-				interesting changes architecturally, or when a small idea is worth
-				a paragraph or two. Think of it less as a feed and more as a
-				slow-moving notebook.
+				Posts here won't be frequent. We'll publish when something about the
+				site genuinely changes, or when a small idea is worth a paragraph or
+				two. Think of it less as a feed and more as a slow-moving notebook.
 			</p>
 			<p>
-				If you're a developer poking around the codebase, the next post on
-				architecture is probably the place to start.
+				If you've just arrived and want the short version of what this place
+				is for, start with "How Catire Time Started" blog post.
 			</p>
 		</>
 	);
